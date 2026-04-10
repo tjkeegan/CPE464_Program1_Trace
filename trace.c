802ea309b2af1519e1fe5fa48f313a85b0955f46
Trevor Keegan <keegan.trevorj@gmail.com>
2026-04-10 16:14:06 -0700

    ip() implementation

--- trace.c
+++ trace.c
@@ -63,7 +63,7 @@ void ethernet(const unsigned char *data) {
         memcpy(type_str, "ARP", 4);
     }
     else if (type == IPV4_TYPE) {
-        memcpy(type_str, "IPv4", 5);
+        memcpy(type_str, "IP", 3);
     }
     else {
         memcpy(type_str, "Unknown", 8);
@@ -80,10 +80,11 @@ void ethernet(const unsigned char *data) {
 
     // parse next header based on type
     if (type == ARP_TYPE) {
-        arp(data);
+        arp(data + ETHERNET_HEADER_LENGTH);
     }
     else if (type == IPV4_TYPE) {
         // parse IPv4 header
+        ip(data + ETHERNET_HEADER_LENGTH);
     }
     // skip unknown packet types
 }
@@ -133,5 +134,68 @@ void arp(const unsigned char *data) {
         "\t\tTarget MAC: %s\n"
         "\t\tTarget IP: %s\n\n",
         opcode_str, sender_mac_str, sender_ip_str, target_mac_str, target_ip_str
+    );
+}
+
+void ip(const unsigned char *data) {
+    // parse IPv4 header
+    uint16_t total_len; // 2 bytes for total length
+    uint8_t version_ihl; // 1 byte for version and IHL
+    uint8_t header_len; // for storing calculated header size
+    uint8_t ttl; // 1 byte for TTL
+    uint8_t protocol; // 1 byte for protocol
+    uint16_t cksum; // 2 bytes for checksum
+    uint8_t sender_ip[IP_LENGTH];
+    uint8_t dest_ip[IP_LENGTH];
+
+    char protocol_str[8]; // enough to hold "Unknown" + null terminator
+    char cksum_str[10]; // enough to hold "Incorrect" + null terminator
+    char sender_ip_str[IP_STR_LENGTH];
+    char dest_ip_str[IP_STR_LENGTH];
+
+    memcpy(&total_len, data + 2, 2);
+    total_len = ntohs(total_len);
+    memcpy(&version_ihl, data, 1);
+    header_len = (version_ihl & 0x0f) * 4;
+    memcpy(&ttl, data + 8, 1);
+    memcpy(&protocol, data + 9, 1);
+    memcpy(&cksum, data + 10, 2);
+    cksum = ntohs(cksum);
+    memcpy(sender_ip, data + 12, IP_LENGTH);
+    memcpy(dest_ip, data + 16, IP_LENGTH);
+
+    if (protocol == 1) {
+        memcpy(protocol_str, "ICMP", 5);
+    }
+    else if (protocol == 6) {
+        memcpy(protocol_str, "TCP", 4);
+    }
+    else if (protocol == 17) {
+        memcpy(protocol_str, "UDP", 4);
+    }
+    else {
+        memcpy(protocol_str, "Unknown", 8);
+    }
+
+    if (in_cksum((unsigned short *)data, header_len) == 0) {
+        memcpy(cksum_str, "Correct", 8);
+    } else {
+        memcpy(cksum_str, "Incorrect", 10);
+    }
+ 
+    inet_ntop(AF_INET, sender_ip, sender_ip_str, IP_STR_LENGTH);
+    inet_ntop(AF_INET, dest_ip, dest_ip_str, IP_STR_LENGTH);
+
+    // print info
+    printf(
+        "\n\tIP Header\n"
+        "\t\tIP PDU Len: %d\n"
+        "\t\tHeader Len (bytes): %d\n"
+        "\t\tTTL: %d\n"
+        "\t\tProtocol: %s\n"
+        "\t\tChecksum: %s (0x%04x)\n"
+        "\t\tSender IP: %s\n"
+        "\t\tDest IP: %s\n\n",
+        total_len, header_len, ttl, protocol_str, cksum_str, cksum, sender_ip_str, dest_ip_str
     );
 }
--- trace.h
+++ trace.h
@@ -6,13 +6,14 @@
 #include <sys/types.h>
 #include <netinet/ether.h>
 #include <arpa/inet.h>
+#include "checksum.h"
 
 #define MAC_LENGTH 6
 #define MAC_STR_LENGTH 18
 #define IP_LENGTH 4
 #define IP_STR_LENGTH 16
 #define ETHERNET_HEADER_LENGTH 14
-#define OPCODE_OFFSET (ETHERNET_HEADER_LENGTH + 6)
+#define OPCODE_OFFSET 6
 #define SENDER_OFFSET (OPCODE_OFFSET + 2)
 #define TARGET_OFFSET (SENDER_OFFSET + MAC_LENGTH + IP_LENGTH)
 #define ARP_TYPE 0x0806
@@ -20,3 +21,4 @@
 
 void ethernet(const unsigned char *data);
 void arp(const unsigned char *data);
+void ip(const unsigned char *data);
